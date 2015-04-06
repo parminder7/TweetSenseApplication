648f8bd883c0d0cce0e73a86f1d37569a110be80
parminder <parminder_jk@hotmail.com>
2015-04-06 13:20:12 -0700

    Sentiment analysis module added using stanford corenlp API

--- src/Main.cs
+++ src/Main.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TweetSenseApplication.src.twitter;
 using TweetSenseApplication.src.fileMgr;
+using TweetSenseApplication.src.analyser;
 
 namespace TweetSenseApplication
 {
@@ -27,7 +28,14 @@ namespace TweetSenseApplication
             //var file = FileManager.GetFileName("worldcup");
             //FileManager.Writer(encodedTweets, file);
 
-            newStreamTweet.tweetStreamer("obama");
+            //newStreamTweet.tweetStreamer("obama");
+
+            Console.WriteLine(SentimentAnalyser.findSentiment("I love to be a part of team"));
+            Console.WriteLine(SentimentAnalyser.findSentiment("I am good but not so good"));
+            Console.WriteLine(SentimentAnalyser.findSentiment("LOL!"));
+            Console.WriteLine(SentimentAnalyser.findSentiment("on cloud 9!"));
+            Console.WriteLine(SentimentAnalyser.findSentiment("I actually didn't like it"));
+            Console.WriteLine(SentimentAnalyser.findSentiment("VIDEO: Today @Manuel_Neuer turns 29 who won the adidas Golden Glove at the 2014 #WorldCup\nhttp://t.co/3q80O4VmBe http://t.co/A6u2i89Xuv"));
 
             Console.ReadKey();
         }
---
+++ src/analyser/SentimentAnalyser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using edu.stanford.nlp.pipeline;
+using edu.stanford.nlp.sentiment;
+using java.util;
+using edu.stanford.nlp.ling;
+using edu.stanford.nlp.util;
+using edu.stanford.nlp.trees;
+using edu.stanford.nlp.neural.rnn;
+using edu.stanford.nlp.parser.lexparser;
+
+
+namespace TweetSenseApplication.src.analyser
+{
+    class SentimentAnalyser
+    {
+
+        public static string findSentiment (String line)
+        {
+        //    // Path to models extracted from `stanford-parser-3.5.1-models.jar`
+        //    var jarRoot = @"..\..\..\..\paket-files\nlp.stanford.edu\stanford-parser-full-2015-01-30\models\";
+        //    var modelsDirectory = jarRoot + @"\edu\stanford\nlp\models";
+            //var modelsDirectory = @"\edu\stanford\nlp\models";
+        
+            var jarRoot = @"..\..\src\stanford-corenlp\";
+            //var modelsDirectory = jarRoot + @"\edu\stanford\nlp\models";            
+            // Loading english PCFG parser from file
+            //var lp = LexicalizedParser.loadModel(modelsDirectory + @"\lexparser\englishPCFG.ser.gz");
+
+            Properties prop = new Properties();
+            prop.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+
+            var curDir = Environment.CurrentDirectory;
+            System.IO.Directory.SetCurrentDirectory(jarRoot);
+            
+            StanfordCoreNLP pipeline = new StanfordCoreNLP(prop);
+            System.IO.Directory.SetCurrentDirectory(curDir);
+
+            String[] polarity = { "Very Negative", "Negative", "Neutral", "Positive", "Very Positive" };
+            int score = 0; 
+
+            if ((line != null) && (line.Length > 0))
+            {
+                Annotation annotation = new Annotation(line);
+                pipeline.annotate(annotation);
+
+                foreach (CoreMap sent in (dynamic)annotation.get(new CoreAnnotations.SentencesAnnotation().getClass()))
+                {
+                    Tree tree = (Tree)sent.get(new SentimentCoreAnnotations.AnnotatedTree().getClass());
+                    score = RNNCoreAnnotations.getPredictedClass(tree);
+                    //Console.WriteLine("The polarity of the satement is "+polarity[score]);
+                }
+            }
+            return polarity[score];
+        }
+    }
+}
